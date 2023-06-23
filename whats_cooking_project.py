09daa029dd2f26baac7479dbf53ca73e6f53fb79
sakshikarande26 <110625006+sakshikarande26@users.noreply.github.com>
2023-06-23 11:09:50 +0530

    Update whats_cooking_project.py

--- whats_cooking_project.py
+++ whats_cooking_project.py
@@ -1,30 +1,34 @@
 import pandas as pd
-df=pd.read_csv("/Users/sakshikarande/Desktop/whats_cooking_internship/sample_submission.csv")
-#print(df)
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble  import RandomForestClassifier
 
-#defining X and Y
-X = df.drop('cuisine', axis=1)
-Y = df['cuisine']
-#print(X)
-#print(Y)
+#creating alias
+rf=RandomForestClassifier(random_state=1)
+lr=LogisticRegression()
+nb=MultinomialNB()
+dt=DecisionTreeClassifier(random_state=0)
 
-#identifying outliers
-import matplotlib.pyplot as plt
-import seaborn as sns
-sns.boxplot(X['id'])
-plt.show()
+d_c =["moroccan","korean","japanese","vietnamese","brazilian","southern_us","british","cajun_creole","chinese","filipino","french","greek","indian","spanish","irish","russian","noroccan","italian","thai","mexican","jamaican"]
+df=pd.read_json("C:/Users/admin/Downloads/whats-cooking/train.json/train.json")
 
-#feature selection
-from sklearn.ensemble import ExtraTreesClassifier
-import matplotlib.pyplot as plt
+#defining X and Y 
+print(df["cuisine"].unique())
+x=df['ingredients']
+y=df['cuisine'].apply(d_c.index)
 
-model = ExtraTreesClassifier()
-model.fit(X,Y)
-print(model.feature_importances_)
+df['all_ingredients']=df['ingredients'].map(";".join)
+cv=CountVectorizer()
+x=cv.fit_transform(df['all_ingredients'].values)
 
-feat_importance = pd.Series(model.feature_importances_,index=X.columns)
-feat_importance.nlargest(4).plot(kind='bar')
-plt.show()
+#train data 
+X_train,X_test,y_train,y_test=train_test_split(x,y,random_state=0,test_size=0.3)
 
-#train data
-'''
+rf.fit(X_train,y_train)
+r_pred=rf.predict(X_test)
+
+print("Random Forest:",accuracy_score(y_test,r_pred))
